c08a5d94b00d6ece3c4f7937cfda44bf31bd672f
Agam-S <singh.agam0009@gmail.com>
2021-08-23 10:24:03 +1000

    2 files modified, 1 added, 1 deleted

--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
 import './style.css';
-// TODO: required imports
-import { Countries } from './models/Countries.enum';
 import { Country } from './models/Country';
-import { IResult } from './models/IResult';
-import { Medals } from './models/Medals.enum';
+import { Result } from './models/Result';
+import { Countries } from './models/Countries.enum';
 import { Sports } from './models/Sports.enum';
+import { Medals } from './models/Medals.enum';
 
 const countrySelect: HTMLSelectElement = <HTMLSelectElement>(
   document.getElementById('country-slt')
@@ -17,57 +16,71 @@ const sportSelect: HTMLSelectElement = <HTMLSelectElement>(
 );
 
 const addButton: HTMLElement = document.getElementById('add-btn');
-//TODO: add an eventlistener to the button to trigger addMedal
+
+addButton.addEventListener('click', addMedal);
 
 let countries: Array<Country> = [];
 
 init();
 
-// This function sets up some display elements
 function init() {
   let count = 0;
   for (let c in Countries) {
     if (isNaN(Number(c))) {
-      let newOption: HTMLOptionElement = document.createElement('option');
-      newOption.innerHTML = c;
-      newOption.value = count.toString();
+      let countryOption: HTMLOptionElement = document.createElement('option');
+      countryOption.innerHTML = c;
+      countryOption.value = count.toString();
       count++;
-      countrySelect.add(newOption);
+      countrySelect.add(countryOption);
     }
   }
 
-  //TODO: populate the Sport select
-  let count1 = 0;
+  let sports = 0;
   for (let s in Sports) {
     if (isNaN(Number(s))) {
-      let newOption: HTMLOptionElement = document.createElement('option');
-      newOption.innerHTML = s;
-      newOption.value = count1.toString();
-      count1++;
-      sportSelect.add(newOption);
+      let sportsOption: HTMLOptionElement = document.createElement('option');
+      sportsOption.innerHTML = s;
+      sportsOption.value = sports.toString();
+      sports++;
+      sportSelect.add(sportsOption);
     }
   }
-  //TODO: populate the Medal select
-  let count2 = 0;
+
+  let medal = 0;
   for (let m in Medals) {
     if (isNaN(Number(m))) {
-      let newOption: HTMLOptionElement = document.createElement('option');
-      newOption.innerHTML = m;
-      newOption.value = count2.toString();
-      count2++;
-      medalSelect.add(newOption);
+      let medaloption: HTMLOptionElement = document.createElement('option');
+      medaloption.innerHTML = m;
+      medaloption.value = String(m);
+      medal++;
+      medalSelect.add(medaloption);
     }
   }
 }
 
-// This function adds a medal to the countries tally
 function addMedal() {
-  //TODO: complete this function
+  let countryexists: boolean = false;
+  for (let i = 0; i < countries.length; i++) {
+    if (countries[i].name == String(Countries[countrySelect.value])) {
+      countryexists = true;
+    }
+  }
+  if (!countryexists) {
+    countries.push(new Country(Countries[countrySelect.value]));
+  }
+
+  for (let i = 0; i < countries.length; i++) {
+    if (countries[i].name == String(Countries[countrySelect.value])) {
+      countries[i].results.push({
+        sport: Sports[sportSelect.value],
+        medal: Medals[medalSelect.value]
+      });
+    }
+  }
 
   displayTable();
 }
 
-// This function refreshes the medal tally table
 function displayTable() {
   const resultsBody: HTMLTableSectionElement = <HTMLTableSectionElement>(
     document.getElementById('results-body')
@@ -78,8 +91,43 @@ function displayTable() {
   );
   newBody.id = 'results-body';
 
-  // TODO: create the rows required for the new table body element
+  for (let i = 0; i < countries.length; i++) {
+    let row = document.createElement('tr');
 
-  // replaces the old tbody with the new one created above
+    let country = document.createElement('td');
+    let countryNames = document.createTextNode(countries[i].name);
+    country.appendChild(countryNames);
+    row.appendChild(country);
+
+    let gold = document.createElement('td');
+    let goldMedal = document.createTextNode(
+      String(countries[i].totalMedalType(Medals['Gold']))
+    );
+    gold.appendChild(goldMedal);
+    row.appendChild(gold);
+
+    let silver = document.createElement('td');
+    let silverMedal = document.createTextNode(
+      String(countries[i].totalMedalType(Medals['Silver']))
+    );
+    silver.appendChild(silverMedal);
+    row.appendChild(silver);
+
+    let bronze = document.createElement('td');
+    let bronzeMedal = document.createTextNode(
+      String(countries[i].totalMedalType(Medals['Bronze']))
+    );
+    bronze.appendChild(bronzeMedal);
+    row.appendChild(bronze);
+
+    let total = document.createElement('td');
+    let totalmedals = document.createTextNode(
+      String(countries[i].totalMedals())
+    );
+    total.appendChild(totalmedals);
+    row.appendChild(total);
+
+    newBody.appendChild(row);
+  }
   resultsBody.parentNode.replaceChild(newBody, resultsBody);
 }
--- models/Country.ts
+++ models/Country.ts
@@ -1,20 +1,26 @@
-import { IResult } from './IResult';
+import { Result } from './Result';
 import { Medals } from './Medals.enum';
 
 export class Country {
-  // TODO: Country requires a name attribute and a results attribute.
-  // name is a string, results is an array of Result
+  name: string;
+  results: Array<IResult>;
 
-  // TODO: receives a name and initialises the results array to an empty array
-  constructor(name: string) {}
-
-  // return the total number of medals
-  totalMedals(): number {
-    //TODO: complete
+  constructor(name: string) {
+    this.name = name;
+    this.results = [];
   }
 
-  // given a medal type, return the amount of that type of medal
+  totalMedals(): number {
+    return this.results.length;
+  }
+
   totalMedalType(medal: Medals): number {
-    // TODO:
+    let counter: number = 0;
+    for (let i = 0; i < this.results.length; i++) {
+      if (this.results[i].medal == medal) {
+        counter++;
+      }
+    }
+    return counter;
   }
 }
